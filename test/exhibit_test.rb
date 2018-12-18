cba289f2f9a6455795dc86f5673c01e5cb405b71
JennicaStiehl <jennica.stiehl@gmail.com>
2018-12-18 09:16:14 -0700

    adds iteration 1

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -1,3 +1,5 @@
+require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/exhibit'
 require './lib/patron'
 
@@ -6,5 +8,11 @@ class ExhibitTest < Minitest::Test
   def test_it_exists
     exhibit = Exhibit.new("Gems and Minerals", 0)
     assert_instance_of Exhibit, exhibit
+  end
+
+  def test_it_has_attributes
+    exhibit = Exhibit.new("Gems and Minerals", 0)
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
   end
 end
